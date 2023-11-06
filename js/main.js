7c020cbe61e914d4de24a73eb07c52e1fbca67a5
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-06 16:22:36 +0100

    feat: calcolo prezzo scontato

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ console.log("L'età del passeggero è: ", etaPasseggero);
 // Calcolo prezzo totale del biglietto
 const prezzoTotale = numeroKm * 0.21;
 console.log("Il prezzo totale del biglietto è: ", prezzoTotale);
+
+// Dichiarazione/assegnazione variabile prezzo scontato
+let prezzoScontato = 0;
+
+// Calcolo prezzo scontato
+if (etaPasseggero <= 18) {
+  prezzoScontato = prezzoTotale - (prezzoTotale * 20) / 100;
+  console.log("Il prezzo del biglietto scontato è: ", prezzoScontato);
+} else if (etaPasseggero > 65) {
+  prezzoScontato = prezzoTotale - (prezzoTotale * 40) / 100;
+  console.log("Il prezzo del biglietto scontato è: ", prezzoScontato);
+} else {
+  prezzoScontato = prezzoTotale;
+}
