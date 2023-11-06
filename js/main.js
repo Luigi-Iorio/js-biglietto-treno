5c1ad3060a0364087a0e6756abec2299d51c8672
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-06 18:31:50 +0100

    refactor: creata variabile per messagio in console

--- js/main.js
+++ js/main.js
@@ -7,28 +7,16 @@ const etaPasseggero = parseFloat(prompt("Inserisci la tua età"));
 console.log("L'età del passeggero è: ", etaPasseggero);
 
 // Calcolo prezzo totale del biglietto
-const prezzoTotale = numeroKm * 0.21;
-console.log(
-  "Il prezzo totale del biglietto è: ",
-  parseFloat(prezzoTotale.toFixed(2))
-);
-
-// Dichiarazione/assegnazione variabile prezzo scontato
-let prezzoScontato = 0;
+let prezzoTotale = numeroKm * 0.21; //prezzo predefinito
+let messaggio = "Il prezzo totale del biglietto è: "; //messaggio predefinito
 
 // Calcolo prezzo scontato
-if (etaPasseggero <= 18) {
-  prezzoScontato = prezzoTotale - (prezzoTotale * 20) / 100;
-  console.log(
-    "Il prezzo del biglietto scontato è: ",
-    parseFloat(prezzoScontato.toFixed(2))
-  );
+if (etaPasseggero < 18) {
+  prezzoTotale = prezzoTotale - (prezzoTotale * 20) / 100;
+  messaggio = "Il prezzo scontato del 20% è: ";
 } else if (etaPasseggero > 65) {
-  prezzoScontato = prezzoTotale - (prezzoTotale * 40) / 100;
-  console.log(
-    "Il prezzo del biglietto scontato è: ",
-    parseFloat(prezzoScontato.toFixed(2))
-  );
-} else {
-  prezzoScontato = prezzoTotale;
+  prezzoTotale = prezzoTotale - (prezzoTotale * 40) / 100;
+  messaggio = "Il prezzo scontato del 40% è: ";
 }
+
+console.log(messaggio, parseFloat(prezzoTotale.toFixed(2)));
