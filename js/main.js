56796ad0428ba0c20e12544fafe33e6150e67196
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-06 16:49:34 +0100

    feat: numeri con 2 decimali

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // Richiesta - km da percorrere
-const numeroKm = parseInt(prompt("Inserisci il numero di km da percorrere"));
+const numeroKm = parseFloat(prompt("Inserisci il numero di km da percorrere"));
 console.log("Il numero di km da percorrere è: ", numeroKm);
 
 // Richiesta - età del passeggero
-const etaPasseggero = parseInt(prompt("Inserisci la tua età"));
+const etaPasseggero = parseFloat(prompt("Inserisci la tua età"));
 console.log("L'età del passeggero è: ", etaPasseggero);
 
 // Calcolo prezzo totale del biglietto
 const prezzoTotale = numeroKm * 0.21;
-console.log("Il prezzo totale del biglietto è: ", prezzoTotale);
+console.log(
+  "Il prezzo totale del biglietto è: ",
+  parseFloat(prezzoTotale.toFixed(2))
+);
 
 // Dichiarazione/assegnazione variabile prezzo scontato
 let prezzoScontato = 0;
@@ -16,10 +19,16 @@ let prezzoScontato = 0;
 // Calcolo prezzo scontato
 if (etaPasseggero <= 18) {
   prezzoScontato = prezzoTotale - (prezzoTotale * 20) / 100;
-  console.log("Il prezzo del biglietto scontato è: ", prezzoScontato);
+  console.log(
+    "Il prezzo del biglietto scontato è: ",
+    parseFloat(prezzoScontato.toFixed(2))
+  );
 } else if (etaPasseggero > 65) {
   prezzoScontato = prezzoTotale - (prezzoTotale * 40) / 100;
-  console.log("Il prezzo del biglietto scontato è: ", prezzoScontato);
+  console.log(
+    "Il prezzo del biglietto scontato è: ",
+    parseFloat(prezzoScontato.toFixed(2))
+  );
 } else {
   prezzoScontato = prezzoTotale;
 }
