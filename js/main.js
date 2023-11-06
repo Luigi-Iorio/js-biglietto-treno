c2909fb90ff011fe137bf881cf5d5c401f5418a3
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-06 15:42:28 +0100

    feat: richieste all'utente + calcolo prezzo totale

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Richiesta - km da percorrere
+const numeroKm = parseInt(prompt("Inserisci il numero di km da percorrere"));
+console.log("Il numero di km da percorrere è: ", numeroKm);
+
+// Richiesta - età del passeggero
+const etaPasseggero = parseInt(prompt("Inserisci la tua età"));
+console.log("L'età del passeggero è: ", etaPasseggero);
+
+// Calcolo prezzo totale del biglietto
+const prezzoTotale = numeroKm * 0.21;
+console.log("Il prezzo totale del biglietto è: ", prezzoTotale);
